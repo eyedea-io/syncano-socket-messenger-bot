afcbdd0aa3aa5418063e414a38687e4d428666b8
mkucharz <m@kucharz.net>
2017-03-22 15:32:02 +0100

    WIP.

--- scripts/webhook.js
+++ scripts/webhook.js
@@ -1,26 +1,25 @@
-import connect from 'syncano-server'
-import {debug} from './helpers/debug';
+import {logger, event, response} from 'syncano-server'
 
-const s = connect() // Connect to Syncano
+
+const {debug} = logger('webhook.js');
 
 if (ARGS['hub.mode'] === 'subscribe') {
   if (ARGS['hub.verify_token'] === 'messenger-bot') {
-    setResponse(new HttpResponse(200, ARGS['hub.challenge'], 'text/plain'));
+    response(ARGS['hub.challenge']);
   } else {
-    setResponse(new HttpResponse(400, 'Wrong token!', 'text/plain'));
+    response('Wrong token!', 400);
   }
 }
 
 if (ARGS.entry && ARGS.entry[0] && ARGS.entry[0].messaging) {
   const messaging_events = ARGS.entry[0].messaging;
   for (let i = 0; i < messaging_events.length; i++) {
-      let event = messaging_events[i]
-      let sender = event.sender.id
+      const event = messaging_events[i]
+      const sender = event.sender.id
       if (event.message && event.message.text) {
-          let text = event.message.text
-          // Sending event
-          debug('m-bot-msg-rec', JSON.stringify({text, sender}))
-          s.event.emit('m-bot-msg-rec', {text, sender})
+          const text = event.message.text
+          debug('Sending event', {text, sender})
+          s.event.emit('message-received', {text, sender})
       }
   }
 }
