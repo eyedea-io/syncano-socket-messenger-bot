6b627e0427bcf0f2f6d37ba6b20834bfaad7763f
mkucharz <m@kucharz.net>
2017-04-27 12:59:53 +0200

    wip.

--- scripts/webhook.js
+++ scripts/webhook.js
@@ -1,6 +1,6 @@
 import {logger, event, response} from 'syncano-server'
 
-
+console.log("XXX", logger, event, response)
 const {debug} = logger('webhook.js');
 
 if (ARGS['hub.mode'] === 'subscribe') {
@@ -14,12 +14,12 @@ if (ARGS['hub.mode'] === 'subscribe') {
 if (ARGS.entry && ARGS.entry[0] && ARGS.entry[0].messaging) {
   const messaging_events = ARGS.entry[0].messaging;
   for (let i = 0; i < messaging_events.length; i++) {
-      const event = messaging_events[i]
-      const sender = event.sender.id
-      if (event.message && event.message.text) {
-          const text = event.message.text
+      const fbEvent = messaging_events[i]
+      const sender = fbEvent.sender.id
+      if (fbEvent.message && fbEvent.message.text) {
+          const text = fbEvent.message.text
           debug('Sending event', {text, sender})
-          s.event.emit('message-received', {text, sender})
+          event.emit('message-received', {text, sender})
       }
   }
 }
